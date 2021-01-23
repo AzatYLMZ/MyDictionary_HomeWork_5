b80defdc93d142c16455f3dca4924c135a3a716e
Azat Yılmaz <Azat.Ylmz@hotmail.com>
2021-01-24 00:10:30 +0300

    Add project files.

---
+++ HomeWork_5/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T_k, T_v>
+    {
+        T_k[] _key;
+        T_k[] _tempKey;
+        T_v[] _value;
+        T_v[] _tempValue;
+
+        public MyDictionary()
+        {
+            _key = new T_k[0];
+            _value = new T_v[0];
+        }
+        public void Add(T_k item1, T_v item2)
+        {
+            _tempKey = _key;
+            _key = new T_k[_key.Length + 1];
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+            }
+            _key[_key.Length - 1] = item1;
+
+            _tempValue = _value;
+            _value = new T_v[_value.Length + 1];
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+            _value[_value.Length - 1] = item2;
+        }
+        public int Count
+        {
+            get { return _key.Length + _value.Length; }
+        }
+        public void Yazdir()
+        {
+            Console.WriteLine("Öğrenciler\n");
+            for (int i = 0; i < _key.Length; i++)
+            {
+                Console.WriteLine("  " + _key[i] + ": " + _value[i]);
+            }
+        }
+
+    }
+}
---
+++ HomeWork_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyDictionary;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> ogrenciler = new MyDictionary<string, int>();
+
+            ogrenciler.Add("Azat", 15);
+            ogrenciler.Add("Berat", 16);
+            ogrenciler.Add("Rojda", 25);
+
+
+            ogrenciler.Yazdir();
+        }
+    }
+}
